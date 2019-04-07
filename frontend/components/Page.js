2d65418ef7db68f97a9458744f28a6c3d68eccd6
akhanaton <akhanaton@gmail.com>
2019-04-07 15:00:16 +0200

    wire up react to graphql, list items from db

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -52,7 +52,6 @@ const Inner = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
-  background: red;
 `;
 
 class Page extends Component {
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,19 +1,33 @@
 import App, { Container } from 'next/app';
+import { ApolloProvider } from 'react-apollo';
 import Page from '../components/Page';
 
+import withData from '../lib/withData';
+
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
     return (
       <div>
         <Container>
-          <Page>
-            <Component />
-          </Page>
+          <ApolloProvider client={apollo}>
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </ApolloProvider>
         </Container>
       </div>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
